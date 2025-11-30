d316a16fda47eb7e5a0f62f08547081b6863aab5
hayato <jxy0809@gmail.com>
2025-11-30 13:18:35 +0800

    feat: convert miniprogram index page to WebView shell
    - Replace static page with web-view component
    - Load https://jxy0809.github.io/AIDANCE/ in WebView
    - Add WebView message listener
    - Optimize page styles for full-screen WebView
    - Disable pull-down refresh for better UX

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,9 +1,15 @@
 Page({
   data: {
-    
+    webUrl: 'https://jxy0809.github.io/AIDANCE/'
   },
   
   onLoad: function(options) {
-    console.log('AIDANCE 小程序启动')
+    console.log('AIDANCE WebView 启动')
+    console.log('加载页面:', this.data.webUrl)
+  },
+
+  // WebView 消息监听
+  onMessage: function(e) {
+    console.log('收到 WebView 消息:', e.detail.data)
   }
 })
